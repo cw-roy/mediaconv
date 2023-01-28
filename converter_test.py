9fd29bf9c39b414676addadbb1f1e5bf81b8e966
cw-roy <curtis_roy@msn.com>
2023-01-28 14:56:24 -0800

    Update .gitignore, converter_test.py

--- converter_test.py
+++ converter_test.py
@@ -6,12 +6,12 @@ from pyffmpeg import FFmpeg
 
 ff = FFmpeg()
 
+
 class FFMConverter:
     """FFmpeg class"""
 
     def to_mp4_pyffmpeg(self, input_file, output_file):
-        """Using pyffmpeg's native method.  No modifications, other than
-        format conversion."""
+        """Using pyffmpeg's native '.convert' method. Reduces file size"""
         begin = time.perf_counter()
         try:
             ff.convert(input_file, output_file)
@@ -23,13 +23,14 @@ class FFMConverter:
         )
 
     def to_mp4_options(self, input_file, output_file):
-        """Using .options pyffmpeg method, resized. Experimental only to
-        compare ff.options method versus straight ff.convert method.
-        Resizing probably won't be done in production. However, using the
-        .options method provides the option for more granular detail."""
+        """Using .options pyffmpeg method.  This changes encoding of both
+        video and audio streams.  Experimental only to compare ff.options
+        method versus straight ff.convert method. Manipulation probably
+        won't be done in production. However, using the .options method
+        provides the ability to specify more granular detail."""
         begin = time.perf_counter()
         try:
-            ff.options(f"-i {input_file} -vf scale=720:-1 -c:a copy {output_file}")
+            ff.options(f"-i {input_file} -c:v libx264 -c:a aac {output_file}")
         except BaseException:
             print("Error in options function")
         end = time.perf_counter()
@@ -37,7 +38,25 @@ class FFMConverter:
             f"File {input_file} converted to {output_file} in {end - begin:0.2f} seconds."
         )
 
+    def to_mp4_options_copy(self, input_file, output_file):
+        """Using a specific 'copy' .options method to directly transcode original
+        video and audio. Seems to have no effect on file size or quality but
+        processes quite a bit faster than either of the other methods."""
+        begin = time.perf_counter()
+        try:
+            ff.options(f"-i {input_file} -c:v copy -c:a copy {output_file}")
+        except BaseException:
+            print("Error in options_copy function")
+        end = time.perf_counter()
+        print(
+            f"File {input_file} converted to {output_file} in {end - begin:0.2f} seconds."
+        )
+
 
 ffm = FFMConverter()
-ffm.to_mp4_pyffmpeg("sample_3gp.3gp", "converted_3gp.mp4")
-# ffm.to_mp4_options("sample_3gp.3gp", "converted_3gp.mp4")
+
+ffm.to_mp4_pyffmpeg("sample-ogv-file.ogx", "pyffmpeg_converted_ogv.mp4")
+
+ffm.to_mp4_options("sample-ogv-file.ogx", "options_converted_ogv.mp4")
+
+ffm.to_mp4_options_copy("sample-ogv-file.ogx", "options_copyconverted_ogv.mp4")
